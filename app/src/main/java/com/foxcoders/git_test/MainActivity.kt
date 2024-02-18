b8b9939d97d49028a835fec813e1dfb91a5113c1
Rayhan <rayhan.khan@gozayaan.com>
2024-02-18 22:49:22 +0600

    fix- name changed again

--- app/src/main/java/com/foxcoders/git_test/MainActivity.kt
+++ app/src/main/java/com/foxcoders/git_test/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Rayhan Khan 2")
+                    Greeting("Rayhan Kh")
                 }
             }
         }
