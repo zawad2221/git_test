a8da94acacda78184f1fe16d41c0fd40798b7b75
zawad hossain rifat <32180355+zawad2221@users.noreply.github.com>
2024-02-18 23:04:42 +0600

    test commit for change request

--- app/src/main/java/com/foxcoders/git_test/MainActivity.kt
+++ app/src/main/java/com/foxcoders/git_test/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Rayhan Kh")
+                    Greeting("Rayhan Khan")
                 }
             }
         }
